dc8c37cacdf4fb6dc492619c2cf53abe1eb0027f
Andrey Gelman <andrey.gelman@gmail.com>
2025-06-08 20:37:57 +0300

    transport test: socket: one client many servers
    A single client aggregates responses by multiple servers:
    ```
    $ ./transport_test --server 5555 &
    $ ./transport_test --server 5556 &
    $ ./transport_test --server 5557 &
    $ ./transport_test --client 5555 5556 5557
    Result:
     7
     7
     7
    ```
    
    (*) The results, of course, don't have to be identical.
    
    Signed-off-by: Andrey Gelman <andrey.gelman@gmail.com>

---
+++ transport/socket/tcp_multi_client.h
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+/*
+ * TCP transport implementation for RPC client.
+ * A single client can call multiple servers.
+ *
+ * Copyright (c) 2025, Andrey Gelman <andrey.gelman@gmail.com>
+ */
+
+#pragma once
+
+#include "client.h"
+#include "utils.h"
+
+#include <unistd.h>
+
+#include <thread>
+
+
+namespace rpc {
+
+class TcpMultiClient {
+public:
+	TcpMultiClient(const std::string& host, const std::vector<uint16_t>& ports)
+	{
+		for (auto port : ports) {
+			int new_sock = tcp::client_socket(host, port);
+			if (new_sock != -1) {
+				m_socks.insert(new_sock);
+			}
+		}
+	}
+
+	~TcpMultiClient()
+	{
+		for (auto sock : m_socks) {
+			close(sock);
+		}
+	}
+
+	template <typename R, typename... Args>
+	auto call(const std::string& funcID, Args&&... args)
+	{
+		auto [future, buffer, id] = m_client.multi_call<R>(funcID, std::forward<Args>(args)...);
+
+		auto transaction = std::async(std::launch::async, [&] {
+			for (auto sock : m_socks) {
+				tcp::send_buffer(sock, buffer.data(), buffer.size());
+			}
+
+			if constexpr (std::is_same_v<R, void>) {
+				/* no return value, do not wait */
+				return;
+			}
+
+			size_t currentSock = 0;
+			size_t lastSock = m_socks.size() - 1;
+			for (auto sock : m_socks) {
+				auto respBuffer = tcp::recv_buffer(sock);
+
+				try {
+					msgpack::sbuffer resp;
+					resp.write(respBuffer.data(), respBuffer.size());
+					m_client.ingest_resp(resp, (currentSock++ == lastSock)/*last*/);
+				} catch (...) {
+					m_client.cancel(id, std::move(std::current_exception()));
+				}
+			}
+		});
+
+		return future.get();
+	}
+
+private:
+	std::set<int> m_socks;
+	rpc::Client m_client;
+};
+
+}
+
--- transport/socket/test.cpp
+++ transport/socket/test.cpp
@@ -1,4 +1,5 @@
 #include "tcp_client.h"
+#include "tcp_multi_client.h"
 #include "tcp_server.h"
 
 #include <iostream>
@@ -24,6 +25,40 @@ static void rpc_client(int argc, char* argv[])
 		std::cout << "Result: " << result << "\n";
 
 		client.call<void>("print", "Hello, world !");
+	} catch (const std::exception& ex) {
+		std::cerr << "RPC call failed: " << ex.what() << "\n";
+	} catch (...) {
+		std::cerr << "RPC call failed\n";
+	}
+}
+
+static void rpc_multi_client(int argc, char* argv[])
+{
+	std::vector<uint16_t> port{5555};
+
+	if (argc > 2) {
+		try {
+			port.clear();
+
+			for (int i = 2; i < argc; ++i) {
+				port.push_back(static_cast<uint16_t>(std::stoi(argv[i])));
+			}
+		} catch (const std::exception& ex) {
+			std::cerr << "Invalid port number: " << argv[2] << "\n";
+			return;
+		}
+	}
+
+	try {
+		rpc::TcpMultiClient client("127.0.0.1", port);
+
+		std::vector<int> result = client.call<int>("add", 4, 5);
+
+		std::cout << "Result:\n";
+		for (const auto& res : result)
+			std::cout << "  " << res << "\n";
+
+		client.call<void>("print", "Hello, many worlds !");
 	} catch (const std::exception& ex) {
 		std::cerr << "RPC call failed: " << ex.what() << "\n";
 	} catch (...) {
@@ -70,15 +105,21 @@ int main(int argc, char* argv[])
 		return 0;
 	}
 
-	if ((argc > 1) && (std::string(argv[1]) == "--client")) {
+	if ((argc > 1) && (argc < 4) && (std::string(argv[1]) == "--client")) {
+		/* 1 port - implicit or explicit */
 		rpc_client(argc, argv);
+		return 0;
+	}
+
+	if ((argc > 3) && (std::string(argv[1]) == "--client")) {
+		/* 2+ ports specified */
+		rpc_multi_client(argc, argv);
 		return 0;
 	}
 
 	std::cerr << "TCP RPC test\n";
 	std::cerr << "Command line options:\n";
-	std::cerr << "  --server [port]  invoke RPC server\n";
-	std::cerr << "  --client [port]  invoke RPC client\n";
+	std::cerr << "  --server [port]                      invoke RPC server\n";
+	std::cerr << "  --client [port [port [port [...]]]]  invoke RPC client\n";
 	return 1;
 }
-
